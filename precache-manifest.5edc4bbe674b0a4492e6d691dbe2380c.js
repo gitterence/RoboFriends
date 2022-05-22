22ec0c337e6d135b28a8bd7a8928b872227fe679
NerdyTao <yewisc@gmail.com>
2022-05-21 19:20:36 -0700

    Updates

---
+++ precache-manifest.5edc4bbe674b0a4492e6d691dbe2380c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "0556e747a6dbba917516",
+    "url": "/RoboFriends/static/css/main.4fba789a.chunk.css"
+  },
+  {
+    "revision": "0556e747a6dbba917516",
+    "url": "/RoboFriends/static/js/main.9d19e908.chunk.js"
+  },
+  {
+    "revision": "0e1af097a2f3b734e9ae",
+    "url": "/RoboFriends/static/js/runtime~main.b621a328.js"
+  },
+  {
+    "revision": "242e5ee2b6b9514992da",
+    "url": "/RoboFriends/static/css/2.a1c962b8.chunk.css"
+  },
+  {
+    "revision": "242e5ee2b6b9514992da",
+    "url": "/RoboFriends/static/js/2.e23f19c8.chunk.js"
+  },
+  {
+    "revision": "79be80b375553eb66abb",
+    "url": "/RoboFriends/static/js/3.c70b6b5b.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "2c5dbccd253634fcd41ff89f31cc32b9",
+    "url": "/RoboFriends/index.html"
+  }
+];
